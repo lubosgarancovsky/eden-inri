f94fa3063359aba6f4ca9cad38ad7d6b155e504f
Ľuboš Garančovský <lubos.garancovsky@gmail.com>
2026-01-03 04:20:18 +0100

    Add avatar and color to user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -5,11 +5,14 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()" json:"id"`
-	Username  string    `json:"username"`
-	Email     string    `json:"email"`
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
+	ID            uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()" json:"id"`
+	Username      string    `json:"username"`
+	Email         string    `json:"email"`
+	FirstName     string    `json:"firstName"`
+	LastName      string    `json:"lastName"`
+	Color         string    `json:"color"`
+	AvatarVersion *int      `json:"avatarVersion"`
+	AvatarMime    *string   `json:"avatarMime"`
 }
 
 func (User) TableName() string {
