9a38a8cfcf3bbdc83f13be1d513f9aec9e716826
Ľuboš Garančovský <lubos.garancovsky@gmail.com.com>
2026-01-05 11:54:59 +0100

    deploy with systemctl

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 REMOTE_USER=lubos
 REMOTE_HOST=pi
 REMOTE_PATH=/home/lubos/containers/eden/eden-inri
+SERVICE_NAME='eden-inri'
 
 # Files and folders to sync with remote
 FILES_TO_SYNC="build/eden-inri"
@@ -28,7 +29,23 @@ echo "-- Deploying remotely using podman-compose"
 ssh $REMOTE_USER@$REMOTE_HOST << EOF
 cd $REMOTE_PATH
 
-podman-compose up -d --build --force-recreate
+echo "-- Stop and disable service"
+systemctl --user stop container-$SERVICE_NAME.service
+systemctl --user disable container-$SERVICE_NAME.service
+
+echo "-- Building the service image"
+podman-compose up -d --build --no-recreate
+
+echo "-- Generating systemd unit"
+podman generate systemd --name $SERVICE_NAME --files --new
+mkdir -p ~/.config/systemd/user/
+mv container-$SERVICE_NAME.service ~/.config/systemd/user/
+podman stop $SERVICE_NAME
+
+echo "-- Staring and enabling service"
+systemctl --user daemon-reload
+systemctl --user enable container-$SERVICE_NAME.service
+systemctl --user start container-$SERVICE_NAME.service
 
 podman image prune -f
 
