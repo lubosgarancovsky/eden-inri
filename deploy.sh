88bf9c70400f85c483618bb0767df8f9c0c6df57
Ľuboš Garančovský <lubos.garancovsky@gmail.com>
2026-01-06 12:25:18 +0100

    force recreate on deploy

--- deploy.sh
+++ deploy.sh
@@ -34,7 +34,7 @@ systemctl --user stop container-$SERVICE_NAME.service
 systemctl --user disable container-$SERVICE_NAME.service
 
 echo "-- Building the service image"
-podman-compose up -d --build --no-recreate
+podman-compose up -d --build --force-recreate
 
 echo "-- Generating systemd unit"
 podman generate systemd --name $SERVICE_NAME --files --new
