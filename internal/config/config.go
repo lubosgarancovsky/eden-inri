41d1c28ac5d1e1213ce5e6af71af4ff5ddaf7d4d
Ľuboš Garančovský <lubos.garancovsky@gmail.com.com>
2026-01-11 18:18:00 +0100

    return deleted config

---
+++ internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"log"
+
+	"github.com/lubosgarancovsky/go-kit"
+)
+
+var GlobalConfig *Config
+
+type Config struct {
+	ServiceName        string
+	Port               int    `field:"PORT" default:"9092"`
+	DBUrl              string `field:"DB_URL"`
+	UploadsFolder      string `field:"UPLOADS_FOLDER"`
+	TemplatesFolder    string `field:"TEMPLATES_FOLDER"`
+	SMTPHost           string `field:"SMTP_HOST"`
+	SMTPPort           int    `field:"SMTP_PORT"`
+	SMTPFrom           string `field:"SMTP_FROM"`
+	InvitationTokenExp int    `field:"INVITATION_TOKEN_EXP" default:"3600"`
+	InvitationUrl      string `field:"INVITATION_URL"`
+}
+
+func Init(serviceName string) {
+	var appConfig Config
+	if err := go_kit.LoadEnv(&appConfig, ".env", ".env.local"); err != nil {
+		log.Fatal("Failed to load config from .env file", err)
+	}
+
+	appConfig.ServiceName = serviceName
+	GlobalConfig = &appConfig
+}
